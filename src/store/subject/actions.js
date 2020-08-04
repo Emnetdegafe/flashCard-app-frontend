18b5d039de08197c28dbd399b22ece8926e9314b
migueldf10 <migueldf10@gmail.com>
2020-08-04 17:53:36 +0200

    dispatch all the courses and the active subject

--- src/store/subject/actions.js
+++ src/store/subject/actions.js
@@ -1,4 +1,6 @@
 import buildAxios from '../../config/buildAxios'
+export const ADD_USER_SUBJECTS = 'ADD_USER_SUBJECTS'
+export const ADD_ACTIVE_SUBJECT = 'ADD_ACTIVE_SUBJECT'
 
 export const fetchAllSubjets = () => async (dispatch, useState) => {
 
@@ -7,7 +9,26 @@ export const fetchAllSubjets = () => async (dispatch, useState) => {
 	try {
 
 		const response = await buildAxios(token).get('/subjects')
-		console.log(response.data)
+		dispatch({
+			type: ADD_USER_SUBJECTS,
+			payload: response.data.subjects
+
+		})
+	} catch (e) {
+		console.log('error', e)
+	}
+
+}
+
+export const fetchSubjectById = (id) => async (dispatch, useState) => {
+	const { token } = useState().user
+	try {
+		const response = await buildAxios(token).get(`/subjects/${id}`)
+		dispatch({
+			type: ADD_ACTIVE_SUBJECT,
+			payload: response.data
+
+		})
 	} catch (e) {
 		console.log('error', e)
 	}
