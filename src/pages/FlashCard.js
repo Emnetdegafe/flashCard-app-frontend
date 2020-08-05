07fbe34f31cb1c23ab5f4a607b2c190b46d7e1fe
migueldf10 <migueldf10@gmail.com>
2020-08-05 10:39:05 +0200

    flashcard page fetches data

--- src/pages/FlashCard.js
+++ src/pages/FlashCard.js
@@ -1,9 +1,30 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { fetchFlashcardById } from '../store/flashcard/actions'
+import { selectCurrentFlashcard } from '../store/flashcard/selectors'
+import { useDispatch, useSelector } from "react-redux";
 
-export default function FlashCard(){
+export default function FlashCard() {
+    const dispatch = useDispatch();
+    const currentFlashcard = useSelector(selectCurrentFlashcard)
+
+    useEffect(() => {
+        dispatch(fetchFlashcardById(1));
+
+    }, [dispatch]);
+
+    if (!currentFlashcard.title) return null
+
     return (
 
-        <div></div>
+        <div>
+            <h1>{currentFlashcard.title}</h1>
+            <p>{currentFlashcard.question}</p>
+            <h3>Options:</h3>
+            <ul>
+                {currentFlashcard.possibleCards.map((card, index) => <li key={index}>{card.answer}</li>)}
+            </ul>
+            <span>Correct card: {currentFlashcard.correctResponse}</span>
+        </div>
     )
-    
+
 }
