ac55e450ea131d60dd08ecdce0724b5b74f20eee
migueldf10 <migueldf10@gmail.com>
2020-08-05 10:15:25 +0200

    Flashcard selector

--- src/store/flashcard/selectors.js
+++ src/store/flashcard/selectors.js
@@ -3,17 +3,34 @@ function getRandomInt(max) {
 }
 
 export const selectCurrentFlashcard = state => {
-	const random = getRandomInt(state.alternative.length + 1)
 
-	const randomizedFlashcards = state.alternative.splice(random, 0, state.current.question)
+	const random = getRandomInt(state.flashcard.alternatives.length)
+
+	// console.log('random number', random)
+	// console.log('Flashcard state', state.flashcard)
+	// console.log('alternative cards', state.flashcard.alternatives)
+
+	const copyOfAlt = [...state.flashcard.alternatives]
+
+	// console.log('copied alternative', state.flashcard.alternatives)
+
+	copyOfAlt.splice(random, 0, state.flashcard.current)
+
+
+	// console.log('spliced miguel in position 1', state.flashcard.alternatives.splice(1, 0, 'miguel'))
 
 	const flashcard = {
-		title: state.current.name,
-		question: state.current.question,
-		possibleResponses: randomizedFlashcards,
-		correctResponses: random
+		title: state.flashcard.current.name,
+		question: state.flashcard.current.question,
+		possibleCards: copyOfAlt,
+		correctResponse: random
 	}
+	return flashcard
 }
+
+// [wrong, wrong, right, wrong]
+
+// correctone = 2
 
 //  This should respond like
 
