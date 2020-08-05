1e2d4734e1382356e69beffa7b3cc2a31cfe1658
demi-werkzam <d.werkzam@gmail.com>
2020-08-05 18:08:45 +0200

    additional styling for cards

--- src/pages/FlashCard.js
+++ src/pages/FlashCard.js
@@ -1,50 +1,60 @@
-import React, { useEffect, useState } from "react"
-import { fetchFlashcardById } from '../store/flashcard/actions'
-import { selectCurrentFlashcard } from '../store/flashcard/selectors'
+import React, { useEffect, useState } from "react";
+import { fetchFlashcardById } from "../store/flashcard/actions";
+import { selectCurrentFlashcard } from "../store/flashcard/selectors";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from 'react-router-dom'
+import { useParams } from "react-router-dom";
+
+import "./index.css";
 
 export default function FlashCard() {
-    const [selectedOption, set_selectedOption] = useState()
-    const { flashcardId } = useParams()
-    console.log('flashcard ID', flashcardId)
-    const dispatch = useDispatch();
-    const currentFlashcard = useSelector(selectCurrentFlashcard)
+  const [selectedOption, set_selectedOption] = useState();
+  const { flashcardId } = useParams();
+  console.log("flashcard ID", flashcardId);
+  const dispatch = useDispatch();
+  const currentFlashcard = useSelector(selectCurrentFlashcard);
 
-    const handleRadios = (e) => {
-        set_selectedOption(parseInt(e.target.value))
-        console.log(e.target.value)
-    }
+  const handleRadios = (e) => {
+    set_selectedOption(parseInt(e.target.value));
+    console.log(e.target.value);
+  };
 
-    useEffect(() => {
-        dispatch(fetchFlashcardById(flashcardId));
+  useEffect(() => {
+    dispatch(fetchFlashcardById(flashcardId));
+  }, [dispatch, flashcardId]);
 
-    }, [dispatch, flashcardId]);
+  if (!currentFlashcard.title) return null;
 
-    if (!currentFlashcard.title) return null
+  return (
+    <div>
+      <h3>{currentFlashcard.title}</h3>
+      <h1>{currentFlashcard.question}</h1>
+      <h3>Options:</h3>
 
-    return (
-
-        <div>
-            <h1>{currentFlashcard.title}</h1>
-            <p>{currentFlashcard.question}</p>
-            <h3>Options:</h3>
-
-            <form>
-                {currentFlashcard.possibleCards.map((card, index) => {
-                    const selected = index === selectedOption ? true : false
-                    console.log('is selected?', selected)
-                    return < div key={index} >
-                        <label htmlFor={index} >{card.answer}</label>
-                        <input type={'radio'} checked={selected} name={index} id={index} onChange={handleRadios} value={index} />
-                    </div>
-                }
-                )
-
-                }
-            </form>
-            <span>Correct card: {currentFlashcard.correctResponse === selectedOption ? 'WIIIIIII' : 'MEEEE'}</span>
-        </div >
-    )
-
+      <form>
+        {currentFlashcard.possibleCards.map((card, index) => {
+          const selected = index === selectedOption ? true : false;
+          console.log("is selected?", selected);
+          return (
+            <div className="center" key={index}>
+              <label htmlFor={index}>{card.answer}</label>
+              <input
+                type={"radio"}
+                checked={selected}
+                name={index}
+                id={index}
+                onChange={handleRadios}
+                value={index}
+              />
+            </div>
+          );
+        })}
+      </form>
+      <span>
+        Correct card:{" "}
+        {currentFlashcard.correctResponse === selectedOption
+          ? "WIIIIIII"
+          : "MEEEE"}
+      </span>
+    </div>
+  );
 }
--- src/pages/Subject.js
+++ src/pages/Subject.js
@@ -4,8 +4,12 @@ import { selectActiveSubject } from "../store/subject/selectors";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 
+import "./index.css";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
+import Button from "react-bootstrap/Button";
+
+import Anime, { anime } from "react-anime";
 
 export default function MyPage() {
   const { subjectId } = useParams();
@@ -19,22 +23,40 @@ export default function MyPage() {
   if (!activeSubject || !activeSubject.name) return null;
   return (
     <div>
-      <h1>The subject is {activeSubject.name}</h1>
+      <h3>The subject is: </h3>
+      <h1>{activeSubject.name}</h1>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Anime delay={anime.stagger(100)} scale={[0.1, 0.9]}>
+          <Button
+            size="lg"
+            style={{ background: "#031508", border: "#031508" }}
+          >
+            Start to play
+          </Button>
+        </Anime>
+      </div>
       <h3>All flashcards:</h3>
-      {activeSubject.flashcards.map((flashcard) => (
-        <CardGroup
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            width: "18rem",
-            height: "10rem",
-          }}
-        >
-          <Card key={flashcard.id}>
+      <CardGroup
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          flexDirection: "row",
+        }}
+      >
+        {activeSubject.flashcards.map((flashcard) => (
+          <Card
+            style={{
+              height: "250px",
+              width: "200px",
+              margin: "5px",
+              background: "#71CB99",
+            }}
+            key={flashcard.id}
+          >
             <Link to={`/flashcards/${flashcard.id}`}>{flashcard.name}</Link>
           </Card>
-        </CardGroup>
-      ))}
+        ))}
+      </CardGroup>
     </div>
   );
 }
