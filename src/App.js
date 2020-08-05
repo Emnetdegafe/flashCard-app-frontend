886ed5d9c4d3bbeb51538f0a38b46c957e0c5476
demi-werkzam <d.werkzam@gmail.com>
2020-08-05 16:33:11 +0200

    Changed font family

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import MessageBox from "./components/MessageBox";
 import SignUp from "./pages/SignUp";
 import Login from "./pages/Login";
 import { getUserWithStoredToken } from "./store/user/actions";
-import FlashCard from "./pages/FlashCard"
-import Subject from "./pages/Subject"
-import Mypage from "./pages/Mypage"
+import FlashCard from "./pages/FlashCard";
+import Subject from "./pages/Subject";
+import Mypage from "./pages/Mypage";
 import { useDispatch, useSelector } from "react-redux";
 import { selectAppLoading } from "./store/appState/selectors";
 // import { Jumbotron } from "react-bootstrap";
@@ -29,10 +29,8 @@ function App() {
   const dispatch = useDispatch();
   const isLoading = useSelector(selectAppLoading);
 
-
   useEffect(() => {
     dispatch(getUserWithStoredToken());
-
   }, [dispatch]);
 
   return (
@@ -47,6 +45,7 @@ function App() {
         <Route path="/subject/:subjectId" component={Subject} />
         <Route path="/flashcards/:flashcardId" component={FlashCard} />
       </Switch>
+      <div className="footer"></div>
     </div>
   );
 }
