e3158afe2576cd1c8e93d4a9728cbc68bac9f3b6
Emnetdegafe <emnetdegafe@gmail.com>
2020-08-05 12:24:09 +0200

    changed API_URL

--- src/config/buildAxios.js
+++ src/config/buildAxios.js
@@ -1,10 +1,10 @@
-import axios from "axios"
+import axios from "axios";
 
-const API = process.env.API_URL || 'http://localhost:4000'
+const API = process.env.REACT_APP_API_URL || "http://localhost:4000";
 
 export default function buildAxios(jwt) {
-	return axios.create({
-		baseURL: API,
-		headers: jwt ? { Authorization: `Bearer ${jwt}` } : {},
-	})
+  return axios.create({
+    baseURL: API,
+    headers: jwt ? { Authorization: `Bearer ${jwt}` } : {},
+  });
 }
