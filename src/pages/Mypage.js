73aef72fe4d1bccd8d1402cb69cc09364749f067
demi-werkzam <d.werkzam@gmail.com>
2020-08-05 16:38:39 +0200

    Cards for flashcards added

--- src/pages/Mypage.js
+++ src/pages/Mypage.js
@@ -1,33 +1,49 @@
-import React, { useEffect } from "react"
-import { fetchAllSubjets } from '../store/subject/actions'
-import { selectAllSubjects, } from '../store/subject/selectors'
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
+import { fetchAllSubjets } from "../store/subject/actions";
+import { selectAllSubjects } from "../store/subject/selectors";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from 'react-router-dom'
+
+import { Jumbotron } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import CardGroup from "react-bootstrap/CardGroup";
+
 export default function MyPage() {
   const dispatch = useDispatch();
-  const allSubjects = useSelector(selectAllSubjects)
-  console.log('all subjects', allSubjects)
+  const allSubjects = useSelector(selectAllSubjects);
+  console.log("all subjects", allSubjects);
 
   useEffect(() => {
     dispatch(fetchAllSubjets());
-
   }, [dispatch]);
 
-  if (!allSubjects || !allSubjects.length > 0) return null
+  if (!allSubjects || !allSubjects.length > 0) return null;
 
   return (
-
     <div>
       <h1>All subjects</h1>
 
-      <ul>
-        {allSubjects.map((subject) => <li key={subject.id}>
-          <Link to={`/subject/${subject.id}`} >
-            {subject.name}
-          </Link>
-        </li>)}
-      </ul>
+      {allSubjects.map((subject) => (
+        <CardGroup
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            width: "18rem",
+            height: "10rem",
+          }}
+        >
+          <Card style={{ flex: 1 }} key={subject.id}>
+            <Card.Text>
+              <Link to={`/subject/${subject.id}`}>{subject.name}</Link>
+            </Card.Text>
+          </Card>
+        </CardGroup>
+      ))}
     </div>
-  )
-
+  );
 }
--- src/pages/Subject.js
+++ src/pages/Subject.js
@@ -1,34 +1,40 @@
-import React, { useEffect } from "react"
-import { fetchSubjectById } from '../store/subject/actions'
-import { selectActiveSubject } from '../store/subject/selectors'
+import React, { useEffect } from "react";
+import { fetchSubjectById } from "../store/subject/actions";
+import { selectActiveSubject } from "../store/subject/selectors";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams } from "react-router-dom";
+
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
 
 export default function MyPage() {
-    const { subjectId } = useParams()
-    const dispatch = useDispatch();
-    const activeSubject = useSelector(selectActiveSubject)
+  const { subjectId } = useParams();
+  const dispatch = useDispatch();
+  const activeSubject = useSelector(selectActiveSubject);
 
+  useEffect(() => {
+    dispatch(fetchSubjectById(subjectId));
+  }, [subjectId, dispatch]);
 
-
-    useEffect(() => {
-        dispatch(fetchSubjectById(subjectId))
-    }, [subjectId, dispatch])
-
-    if (!activeSubject || !activeSubject.name) return null
-    return (
-
-        <div>
-            <h1>The subject is {activeSubject.name}</h1>
-            <h3>All flashcards:</h3>
-            <ul>
-                {activeSubject.flashcards.map((flashcard) => <li key={flashcard.id}>
-                    <Link to={`/flashcards/${flashcard.id}`} >
-                        {flashcard.name}
-                    </Link>
-                </li>)}
-            </ul>
-        </div>
-    )
-
+  if (!activeSubject || !activeSubject.name) return null;
+  return (
+    <div>
+      <h1>The subject is {activeSubject.name}</h1>
+      <h3>All flashcards:</h3>
+      {activeSubject.flashcards.map((flashcard) => (
+        <CardGroup
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            width: "18rem",
+            height: "10rem",
+          }}
+        >
+          <Card key={flashcard.id}>
+            <Link to={`/flashcards/${flashcard.id}`}>{flashcard.name}</Link>
+          </Card>
+        </CardGroup>
+      ))}
+    </div>
+  );
 }
