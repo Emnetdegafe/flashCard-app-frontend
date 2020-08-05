a8f3d166e2079e9e498bcf2f3ecfe29abd2f5e70
Emnetdegafe <emnetdegafe@gmail.com>
2020-08-05 17:43:39 +0200

    Added subject form

--- src/store/subject/selectors.js
+++ src/store/subject/selectors.js
@@ -1,3 +1,4 @@
 
 export const selectAllSubjects = state => state.subject.allSubjects
 export const selectActiveSubject = state => state.subject.activeSubject
+export const newSubject = state=> state.subject.newSubject
