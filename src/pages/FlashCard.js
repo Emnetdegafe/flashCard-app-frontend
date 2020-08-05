31f177b748d28389071daf4a4bf948aa35a43135
migueldf10 <migueldf10@gmail.com>
2020-08-05 11:32:54 +0200

    added flashcard page

--- src/pages/FlashCard.js
+++ src/pages/FlashCard.js
@@ -1,14 +1,20 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { fetchFlashcardById } from '../store/flashcard/actions'
 import { selectCurrentFlashcard } from '../store/flashcard/selectors'
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from 'react-router-dom'
 
 export default function FlashCard() {
+    const [selectedOption, set_selectedOption] = useState()
     const { flashcardId } = useParams()
     console.log('flashcard ID', flashcardId)
     const dispatch = useDispatch();
     const currentFlashcard = useSelector(selectCurrentFlashcard)
+
+    const handleRadios = (e) => {
+        set_selectedOption(parseInt(e.target.value))
+        console.log(e.target.value)
+    }
 
     useEffect(() => {
         dispatch(fetchFlashcardById(flashcardId));
@@ -23,11 +29,22 @@ export default function FlashCard() {
             <h1>{currentFlashcard.title}</h1>
             <p>{currentFlashcard.question}</p>
             <h3>Options:</h3>
-            <ul>
-                {currentFlashcard.possibleCards.map((card, index) => <li key={index}>{card.answer}</li>)}
-            </ul>
-            <span>Correct card: {currentFlashcard.correctResponse}</span>
-        </div>
+
+            <form>
+                {currentFlashcard.possibleCards.map((card, index) => {
+                    const selected = index === selectedOption ? true : false
+                    console.log('is selected?', selected)
+                    return < div key={index} >
+                        <label htmlFor={index} >{card.answer}</label>
+                        <input type={'radio'} checked={selected} name={index} id={index} onChange={handleRadios} value={index} />
+                    </div>
+                }
+                )
+
+                }
+            </form>
+            <span>Correct card: {currentFlashcard.correctResponse === selectedOption ? 'WIIIIIII' : 'MEEEE'}</span>
+        </div >
     )
 
 }
