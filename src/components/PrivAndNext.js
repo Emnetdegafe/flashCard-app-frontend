0c19e9ebf7411f5dc43783a2c3f6390943edc473
Emnetdegafe <emnetdegafe@gmail.com>
2020-08-06 23:13:53 +0200

    Adding Previous and next buttons/functions

---
+++ src/components/PrivAndNext.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectFlashCards } from "../store/subject/selectors";
+import { selectCurrentFlashcard } from "../store/flashcard/selectors";
+import { useHistory } from "react-router-dom";
+
+export default function PrivAndNext(props) {
+  const history = useHistory();
+
+  const flashcards = useSelector(selectFlashCards);
+  console.log("1234", flashcards);
+  const currentCard = useSelector(selectCurrentFlashcard);
+  const currentCardId = props.currentFlashcardId;
+
+  //   const [next, set_next] = useState(false);
+  //   const [previous, set_previous] = useState(false);
+  const [activeCard, set_activeCard] = useState(currentCardId);
+
+  //   const [index, set_index] = useState(0);
+  if (!flashcards || flashcards.length < 1) {
+    return null;
+  }
+
+  const cardsId = flashcards.map((card) => {
+    return card.id;
+  });
+  console.log("cardsId", cardsId);
+  function handleNext(event) {
+    history.push(`/flashcards/${randomId} `);
+  }
+  const randomId = cardsId[Math.floor(Math.random() * cardsId.length)];
+  console.log("random", randomId);
+
+  function handlePrev(event) {
+    history.goBack();
+  }
+
+  return (
+    <div>
+      <button onClick={(event) => handlePrev(event)}>Privious</button>
+      <button onClick={(event) => handleNext(event)}>Next</button>
+    </div>
+  );
+}
