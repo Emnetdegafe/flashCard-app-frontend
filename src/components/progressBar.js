4ce8b10e36ab45c1186053c082ca5361050965b6
migueldf10 <migueldf10@gmail.com>
2020-08-07 16:22:13 +0200

    Progress bar

--- src/components/progressBar.js
+++ src/components/progressBar.js
@@ -1,94 +1,141 @@
 import React from "react";
-import { PieChart, Pie, Legend, Tooltip } from "recharts";
+import { PieChart, Pie, Legend, Tooltip, Cell } from "recharts";
+import { useSelector } from "react-redux";
+import { selectAllSubjects } from "../store/subject/selectors";
 
-export default function progressBar(props) {
-  const {
-    getStatusTrue,
-    getStatusFalse,
-  } = props;
 
-  if (!getStatusTrue ||
-    !getStatusTrue.length < 1 ||
-    !getStatusFalse ||
-    !getStatusFalse.length < 1
-
-  ) return null
-
-  const subjectWithMostMistakes = getStatusFalse.sort(
-    (a, b) => b.numerOfFalseCards - a.numerOfFalseCards
-  );
-
-  const bestSubjects = getStatusTrue.sort(
-    (a, b) => b.numberOfTrueCards - a.numberOfTrueCards
-  );
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
 
 
-  const MostWrong = [
-    {
-      name: subjectWithMostMistakes[0].nameOftheSubject,
-      value: subjectWithMostMistakes[0].numerOfFalseCards,
-    },
-    {
-      name: subjectWithMostMistakes[1].nameOftheSubject,
-      value: subjectWithMostMistakes[1].numerOfFalseCards,
-    },
-    {
-      name: subjectWithMostMistakes[2].nameOftheSubject,
-      value: subjectWithMostMistakes[2].numerOfFalseCards,
-    },
-    {
-      name: subjectWithMostMistakes[3].nameOftheSubject,
-      value: subjectWithMostMistakes[3].numerOfFalseCards,
-    },
-  ];
+export default function ProgressBar() {
+  const allSubjects = useSelector(selectAllSubjects);
+  if (allSubjects.length < 1) return (
+    <p>Create some lessons to see your statistics</p>
+  )
 
-  const topThreeMostRight = [
-    {
-      name: bestSubjects[0].nameOfSubjectTrue,
-      value: bestSubjects[0].numberOfTrueCards,
-    },
-    {
-      name: bestSubjects[1].nameOfSubjectTrue,
-      value: bestSubjects[1].numberOfTrueCards,
-    },
-    {
-      name: bestSubjects[2].nameOfSubjectTrue,
-      value: bestSubjects[2].numberOfTrueCards,
-    },
-  ];
+  const notEmptySubjects = allSubjects.filter((subject) => subject.flashcards && subject.flashcards.length > 0)
+
+  if (notEmptySubjects.length < 1) return (
+    <p>Add cards to your subjects to see some statistics</p>
+  )
+
+  const getStatusTrue = notEmptySubjects.map((subject) => ({
+    value: subject.flashcards && subject.flashcards.filter((card) => card.status === true).length,
+    name: subject.name,
+  }));
+
+  const getStatusFalse = notEmptySubjects.map((subject) => ({
+    value: subject.flashcards && subject.flashcards.filter((card) => card.status === false || !card.status).length,
+    name: subject.name,
+  }));
+
+
+  // const subjectWithMostMistakes = getStatusFalse.sort(
+  //   (a, b) => b.numerOfFalseCards - a.numerOfFalseCards
+  // );
+
+  // const bestSubjects = getStatusTrue.sort(
+  //   (a, b) => b.numberOfTrueCards - a.numberOfTrueCards
+  // );
+
+
+
+  // const MostWrong = [
+  //   {
+  //     name: subjectWithMostMistakes[0].nameOftheSubject,
+  //     value: subjectWithMostMistakes[0].numerOfFalseCards,
+  //   },
+  //   {
+  //     name: subjectWithMostMistakes[1].nameOftheSubject,
+  //     value: subjectWithMostMistakes[1].numerOfFalseCards,
+  //   },
+  //   {
+  //     name: subjectWithMostMistakes[2].nameOftheSubject,
+  //     value: subjectWithMostMistakes[2].numerOfFalseCards,
+  //   },
+  //   {
+  //     name: subjectWithMostMistakes[3].nameOftheSubject,
+  //     value: subjectWithMostMistakes[3].numerOfFalseCards,
+  //   },
+  // ];
+
+  // const topThreeMostRight = [
+  //   {
+  //     name: bestSubjects[0].nameOfSubjectTrue,
+  //     value: bestSubjects[0].numberOfTrueCards,
+  //   },
+  //   {
+  //     name: bestSubjects[1].nameOfSubjectTrue,
+  //     value: bestSubjects[1].numberOfTrueCards,
+  //   },
+  //   {
+  //     name: bestSubjects[2].nameOfSubjectTrue,
+  //     value: bestSubjects[2].numberOfTrueCards,
+  //   },
+  // ];
 
   //In Which Subject do I make the most mistakes
   //In which Subject am I the best
 
   return (
-    <div>
-      Most answered wrong
-      <PieChart width={400} height={400}>
-        <Pie
-          dataKey="value"
-          isAnimationActive={false}
-          data={MostWrong}
-          cx={200}
-          cy={200}
-          fill="#8884d8"
-        />
+    <>
+      <section className='charts'>
+        <div className='pieHolder'>
+          <h3>
 
-        <Tooltip />
-      </PieChart>
-      Top 3 Best
-      <PieChart width={400} height={400}>
-        <Pie
-          dataKey="value"
-          isAnimationActive={false}
-          data={topThreeMostRight}
-          cx={200}
-          cy={200}
-          fill="#8884d8"
-        />
+            Number of pending flashcards
+    </h3>
 
-        <Tooltip />
-      </PieChart>
-    </div>
+          <PieChart width={300} height={300}>
+            <Legend verticalAlign="top" height={36} />
+            <Pie
+              dataKey="value"
+              isAnimationActive={true}
+              data={getStatusFalse}
+              cx={150}
+              cy={150}
+              labelLine={false}
+              label={true}
+              nameKey='name'
+            >
+              {getStatusFalse.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index]}>{entry.name}</Cell>
+              ))}
+            </Pie>
+
+            <Tooltip />
+          </PieChart>
+        </div>
+        <div className='pieHolder'>
+
+          <h3>
+
+            Number of completed flashcards
+      </h3>
+          <PieChart width={300} height={300}>
+            <Legend verticalAlign="top" height={36} />
+            <Pie
+              dataKey="value"
+              isAnimationActive={true}
+              data={getStatusTrue}
+              cx={150}
+              labelLine={false}
+              cy={150}
+              label={true}
+              nameKey='name'
+
+            >
+              {getStatusTrue.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={COLORS[index]}>{entry.name}</Cell>
+
+              ))}
+            </Pie>
+
+            <Tooltip />
+          </PieChart>
+        </div>
+      </section>
+    </>
   );
 }
