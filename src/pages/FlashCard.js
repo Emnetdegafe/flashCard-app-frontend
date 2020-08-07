3b173063edf1c479e614b54ca2d077e2f46c926a
migueldf10 <migueldf10@gmail.com>
2020-08-07 11:38:18 +0200

    Flashcard layout

--- src/pages/FlashCard.js
+++ src/pages/FlashCard.js
@@ -12,7 +12,7 @@ import { useParams } from "react-router-dom";
 
 import "./index.css";
 import PrivAndNext from "../components/PrivAndNext";
-
+import RoundedButton from '../components/RoundedButton'
 export default function FlashCard() {
   const [selectedOption, set_selectedOption] = useState();
   const { flashcardId } = useParams();
@@ -33,49 +33,57 @@ export default function FlashCard() {
   if (!currentFlashcard.title) return null;
 
   return (
-    <div className="center">
-      <PrivAndNext currentFlashcardId={flashcardId}/>
-      <h1>{currentFlashcard.title}</h1>
-      <p className="flashcard-body">{currentFlashcard.question}</p>
-      <h3>Options:</h3>
+    <div className="wrapper colored">
+      <section>
 
-      <form className="options-form">
-        {alternativeCards.possibleCards.map((card, index) => {
-          const selected = index === selectedOption ? true : false;
+        <PrivAndNext currentFlashcardId={flashcardId} />
+        <h1>{currentFlashcard.title}</h1>
+        <p className="flashcard-body">{currentFlashcard.question}</p>
+      </section>
+      <section>
 
-          const classNames = !Number.isInteger(selectedOption)
-            ? `options-form__response`
-            : selectedOption === alternativeCards.correctResponse &&
-              selectedOption === index
-            ? `options-form__response checked__right`
-            : `options-form__response checked__wrong`;
+        <h3>Options:</h3>
 
-          return (
-            <div key={index} className={classNames}>
-              <label htmlFor={index}>{card.answer}</label>
-              <input
-                type={"radio"}
-                checked={selected}
-                name={index}
-                id={index}
-                onChange={handleRadios}
-                value={index}
-              />
-            </div>
-          );
-        })}
-      </form>
+        <form className="options-form">
+          {alternativeCards.possibleCards.map((card, index) => {
+            const selected = index === selectedOption ? true : false;
 
-      <h3>{currentFlashcard.status ? "Completed" : "Not completed yet"}</h3>
-      <div className="flashcard-buttons">
-        <button
-          onClick={() =>
-            dispatch(updateFlashcardStatus(!currentFlashcard.status))
-          }
-        >
-          Complete now
-        </button>
-      </div>
+            const classNames = !Number.isInteger(selectedOption)
+              ? `options-form__response`
+              : selectedOption === alternativeCards.correctResponse &&
+                selectedOption === index
+                ? `options-form__response checked__right`
+                : `options-form__response checked__wrong`;
+
+            return (
+              <div key={index} className={classNames}>
+                <label htmlFor={index}>{card.answer}</label>
+                <input
+                  type={"radio"}
+                  checked={selected}
+                  name={index}
+                  id={index}
+                  onChange={handleRadios}
+                  value={index}
+                />
+              </div>
+            );
+          })}
+        </form>
+      </section>
+      <section>
+
+        <h3>{currentFlashcard.status ? "Completed" : "Not completed yet"}</h3>
+        <div className="flashcard-buttons">
+          <RoundedButton
+            onClick={() =>
+              dispatch(updateFlashcardStatus(!currentFlashcard.status))
+            }
+          >
+            Complete now
+        </RoundedButton>
+        </div>
+      </section>
     </div>
   );
 }
